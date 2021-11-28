dca5dd712de2f4786e114b2beec47235881b0a88
Ali Chaabane <milik9898>
2021-11-28 20:21:29 +0100

    Added a console log

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-
+console.log("tab music app!!!!")
 require('./routes')(app)
 
 // force : false (mean without drop all tables)
